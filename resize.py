d41fadfccb9a567615474e06d4e9c17c5dea985f
Ben Li <benji112358@gmail.com>
2020-02-23 00:00:28 -0500

    the last one wasn't working - this is

--- resize.py
+++ resize.py
@@ -4,12 +4,44 @@ import numpy as np
 import math
 import sys, time
 
+def main():
+    # Read image
+    img = plt.imread('img_example_lr.png')
+    x = plt.imshow(img)
+    plt.show()
+
+    # Scale factor
+    ratio = 2
+    # 'a' is selected because we have only 7 EQNs and 8 unknowns (including 'a')
+    a = -1/2 # -0.5 is chosen from a PAPER; but, -0.75 might also be okay
+    dst = resizeImage(img, ratio, a)
+    print('Completed!')
+
+    # plot Input vs Output
+    imageDict = {}
+    imageDict["input"] = img
+    imageDict["output"] = dst
+    plotHelper(imageDict, 2, 1)
+
+# ...................................................
+
 '''
-    from the paper:
+    from Keys' paper:
     * h = sampling increment
     * u = mask/convolution-matrix
     * xk = interpolation-node 
 '''
+
+#Plot a dictionary of figures: 
+# https://stackoverflow.com/questions/11159436/multiple-figures-in-a-single-window
+def plotHelper(figures, nrows = 2, ncols=1):
+    fig, axeslist = plt.subplots(ncols=ncols, nrows=nrows)
+    for ind,title in enumerate(figures):
+        axeslist.ravel()[ind].imshow(figures[title], cmap=plt.gray())
+        axeslist.ravel()[ind].set_title(title)
+    plt.tight_layout() # optional
+    plt.show()
+
 
 # Mask (convolution matrix) which is piece-wise
 '''
@@ -17,44 +49,18 @@ import sys, time
 The cubic convolution algorithm is derived from a set of conditions..."
 '''
 def convMask(s,a=-1/2):
-    # piece 1
-    if (abs(s) >=0) & (abs(s) <=1):
+    # not in [-2,2]
+    if not 0 <= s <= 2:
+        return 0
+    # [-1,1]
+    elif (abs(s) >=0) and (abs(s) <=1):
         return (a+2)*(abs(s)**3)-(a+3)*(abs(s)**2)+1
-    # piece 2
-    elif (abs(s) > 1) & (abs(s) <= 2):
+    # (1,2], [-2,-1)
+    else: #(abs(s) > 1) & (abs(s) <= 2)
         return a*(abs(s)**3)-(5*a)*(abs(s)**2)+(8*a)*abs(s)-4*a
-    # piece 3
-    return 0
-
-#Padding
-def padding(img,H,W,C):
-    zimg = np.zeros((H+4,W+4,C))                # buffer
-    zimg[2:H+2,2:W+2,:C] = img                  # original image with 2-px-wide border
-    #Pad the first/last two col and row
-    zimg[0:2,2:W+2,:C]=img[0:1,:,:C]            # top
-    zimg[H+2:H+4,2:W+2,:]=img[H-1:H,:,:]        # bottom
-    zimg[2:H+2,W+2:W+4,:]=img[:,W-1:W,:]        # right
-    zimg[2:H+2,0:2,:C]=img[:,0:1,:C]            # left
-    #Pad missing corner-points
-    zimg[0:2,0:2,:C]=img[0,0,:C]                # top-left
-    zimg[0:2,W+2:W+4,:C]=img[0,W-1,:C]          # top-right
-    zimg[H+2:H+4,0:2,:C]=img[H-1,0,:C]          # bot-left
-    zimg[H+2:H+4,W+2:W+4,:C]=img[H-1,W-1,:C]    # bot-right
-    
-    return zimg
-
-# https://github.com/yunabe/codelab/blob/master/misc/terminal_progressbar/progress.py
-def get_progressbar_str(progress):
-    END = 170
-    MAX_LEN = 30
-    BAR_LEN = int(MAX_LEN * progress)
-    return ('Progress:[' + '=' * BAR_LEN +
-            ('>' if BAR_LEN < MAX_LEN else '') +
-            ' ' * (MAX_LEN - BAR_LEN) +
-            '] %.1f%%' % (progress * 100.))
 
 # Bicubic operation
-def bicubic(img, ratio=2, a=-1/2):
+def resizeImage(img, ratio=2, a=-1/2):
     #Get image size
     H,W,C = img.shape
     print("\n\ninput-image dimensions:", img.shape)
@@ -76,6 +82,7 @@ def bicubic(img, ratio=2, a=-1/2):
     _img[0:2,W+2:W+4,:C] = img[0,W-1,:C]          # top-right
     _img[H+2:H+4,0:2,:C] = img[H-1,0,:C]          # bot-left
     _img[H+2:H+4,W+2:W+4,:C] = img[H-1,W-1,:C]    # bot-right
+    img, _img = _img, img                         # swap so we can plot both at the end for comparison
     # ...............................................................
 
     dH, dW = math.floor(H*ratio), math.floor(W*ratio)
@@ -83,7 +90,6 @@ def bicubic(img, ratio=2, a=-1/2):
     print("output-image dimensions will be: ", dst.shape)
     h = 1/ratio # 'h' = sampling-increment
     inc = 0     # progress bar
-
             
     # draw output-image px-by-px (one color-channel at a time)
     for j in range(dH):
@@ -102,10 +108,10 @@ def bicubic(img, ratio=2, a=-1/2):
                 y4 = math.floor(y) + 2 - y
 
                 # [ u(x1) u(x2) u(x3) u(x4) ]
-                mat_l = np.matrix([[convMask(x1,a),convMask(x2,a),convMask(x3,a),convMask(x4,a)]])
+                mat_l = np.array([[convMask(x1,a),convMask(x2,a),convMask(x3,a),convMask(x4,a)]])
 
                 # [ u(y1) u(y2) u(y3) u(y4) ]
-                mat_r = np.matrix([[convMask(y1,a)],[convMask(y2,a)],[convMask(y3,a)],[convMask(y4,a)]])
+                mat_r = np.array([[convMask(y1,a)],[convMask(y2,a)],[convMask(y3,a)],[convMask(y4,a)]])
 
                 # matrix
                 ''' [ f11 ... f14]
@@ -113,36 +119,15 @@ def bicubic(img, ratio=2, a=-1/2):
                     [ .      .  .]
                     [ f41 ... f44]
                 '''
-                mat_m = np.matrix([
-                                    [img[int(y-y1),int(x-x1),channel],img[int(y-y2),int(x-x1),channel],img[int(y+y3),int(x-x1),channel],img[int(y+y4),int(x-x1),channel]],
-                                    [img[int(y-y1),int(x-x2),channel],img[int(y-y2),int(x-x2),channel],img[int(y+y3),int(x-x2),channel],img[int(y+y4),int(x-x2),channel]],
-                                    [img[int(y-y1),int(x+x3),channel],img[int(y-y2),int(x+x3),channel],img[int(y+y3),int(x+x3),channel],img[int(y+y4),int(x+x3),channel]],
-                                    [img[int(y-y1),int(x+x4),channel],img[int(y-y2),int(x+x4),channel],img[int(y+y3),int(x+x4),channel],img[int(y+y4),int(x+x4),channel]]
-                                ])
+                mat_m = np.ones((4,4))
+                mat_m[0] = [img[int(y-y1),int(x-x1),channel],img[int(y-y2),int(x-x1),channel],img[int(y+y3),int(x-x1),channel],img[int(y+y4),int(x-x1),channel]]
+                mat_m[1] = [img[int(y-y1),int(x-x2),channel],img[int(y-y2),int(x-x2),channel],img[int(y+y3),int(x-x2),channel],img[int(y+y4),int(x-x2),channel]]
+                mat_m[2] = [img[int(y-y1),int(x+x3),channel],img[int(y-y2),int(x+x3),channel],img[int(y+y3),int(x+x3),channel],img[int(y+y4),int(x+x3),channel]]
+                mat_m[3] = [img[int(y-y1),int(x+x4),channel],img[int(y-y2),int(x+x4),channel],img[int(y+y3),int(x+x4),channel],img[int(y+y4),int(x+x4),channel]]
                 
                 # compute cth color-channel value for pixel-(j,i) of output image
                 dst[j, i, channel] = np.dot(np.dot(mat_l, mat_m), mat_r)
-
-                # Print progress
-                inc = inc + 1
-                sys.stderr.write('\r\033[K' + get_progressbar_str(inc/(C*dH*dW)))
-                sys.stderr.flush()
-    sys.stderr.write('\n')
-    sys.stderr.flush()
     return dst
 
-# Read image
-img = plt.imread('img_test_lr.png')
-x = plt.imshow(img)
-plt.show()
-
-# Scale factor
-ratio = 2
-
-# 'a' is selected because we have only 7 EQNs and 8 unknowns (including 'a')
-a = -1/2 # -0.5 is chosen from a PAPER; but, -0.75 might also be okay
-
-dst = bicubic(img, ratio, a)
-print('Completed!')
-y = plt.imshow(dst)
-plt.show()
+if __name__ == '__main__':
+    main()
