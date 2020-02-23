41b88beced56b8a61c752207b7da71bfe7a6cf93
Ben Li <benji112358@gmail.com>
2020-02-23 03:09:19 -0500

    works - weird formatting

--- resize.py
+++ resize.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 
+def ALPHA():
+    return -0.5
+
 def main():
     # Read image
     inputImg = plt.imread('img_example_lr.png')
     x = plt.imshow(list(inputImg))
     plt.show()
-    
-    alpha = -0.5 # -0.5 is chosen from a PAPER; but, -0.75 might be okay
     scaleFactor = 1.5
 
     # call resizing function
-    dst = resizeImage(inputImg, scaleFactor, alpha)
+    dst = resizeImage(inputImg, scaleFactor)
     print('Completed!')
 
     # plotting everything
@@ -47,39 +48,39 @@ def plotHelper(figures, nrows = 2, ncols=1):
 "interpolation kernels can be effectively used to create new interpolation algorithms. 
 The cubic convolution algorithm is derived from a set of conditions..."
 '''
-def convMask(s,a=-1/2):
+def convMask(s):
     # not in [-2,2]
     if not 0 <= s <= 2: 
         return 0
     # [-1,1]
     elif 0 <= abs(s) <=1: 
-        return (a+2)*(abs(s)**3)-(a+3)*(abs(s)**2)+1
+        return (ALPHA()+2)*(abs(s)**3)-(ALPHA()+3)*(abs(s)**2)+1
     # (1,2], [-2,-1)
     else: 
-        return a*(abs(s)**3)-(5*a)*(abs(s)**2)+(8*a)*abs(s)-4*a
+        return ALPHA()*(abs(s)**3)-(5*ALPHA())*(abs(s)**2)+(8*ALPHA())*abs(s)-4*ALPHA()
 
-def decimalPart(num): return int(str(num).split('.')[-1])
-
+def decimalPart(num): 
+    return int(str(num).split('.')[-1])
 
 # Bicubic operation
-def resizeImage(img, ratio=2, a=-0.5):
+def resizeImage(img, scaleFactor=2):
     #Get image size
     H,W,C = img.shape
     print("\n\ninput-image dimensions:", img.shape)
 
-    ''' each original-pixel is used as a centroid of an interpolation-box that goes outwards in 4 dirs;
+    ''' each original-pixel is used as a centroid of an interpolation-box that stretches outwards in 4 dirs;
         thus, we pad all sides of the image with 2-layers of pixels. This fulills 
         the otherwise incomplete interpolation-box when we use pixels on the
         outer rows/cols of the image (boundary cases)
     '''
-    img = np.pad(img, 2, mode='edge')
+    img = np.pad(img, 2, mode='edge') # add 2-layers around img (nearest nighbour)
 
     # ...............................................................
 
-    dH, dW = int((H*ratio)//1), int((W*ratio)//1)
+    dH, dW = int((H*scaleFactor)//1), int((W*scaleFactor)//1)
     dst = np.zeros((dH, dW, 3)) # output buffer
     print("output-image dimensions will be: ", dst.shape)
-    h = 1/ratio # sampling-increment
+    h = 1/scaleFactor # sampling-increment
             
     # draw output-image px-by-px (one color-channel at a time)
     for j in range(dH):
@@ -87,15 +88,32 @@ def resizeImage(img, ratio=2, a=-0.5):
             # Image has 3 color-channels (R,G,B)
             for channel in range(C): 
                 x, y = (i*h+2), (j*h+2)
-                x1, y1, x2, y2 = (x-x//1+1), (y-y//1+1), (x-x//1), (y-y//1)   
-                x3, y3, x4, y4 = (x//1-x+1), (y//1-y+1), (x//1-x+2), (y//1-y+2)
                 mat_m = np.ones((4,4))
-                mat_m[0] = [img[int(y-y1),int(x-x1),channel],img[int(y-y2),int(x-x1),channel],img[int(y+y3),int(x-x1),channel],img[int(y+y4),int(x-x1),channel]]
-                mat_m[1] = [img[int(y-y1),int(x-x2),channel],img[int(y-y2),int(x-x2),channel],img[int(y+y3),int(x-x2),channel],img[int(y+y4),int(x-x2),channel]]
-                mat_m[2] = [img[int(y-y1),int(x+x3),channel],img[int(y-y2),int(x+x3),channel],img[int(y+y3),int(x+x3),channel],img[int(y+y4),int(x+x3),channel]]
-                mat_m[3] = [img[int(y-y1),int(x+x4),channel],img[int(y-y2),int(x+x4),channel],img[int(y+y3),int(x+x4),channel],img[int(y+y4),int(x+x4),channel]]
-                _s1 = np.dot(np.array([[convMask(x1,a),convMask(x2,a),convMask(x3,a),convMask(x4,a)]]), mat_m)
-                dst[j, i, channel] = np.dot(_s1,np.array([[convMask(y1,a)],[convMask(y2,a)],[convMask(y3,a)],[convMask(y4,a)]]))
+                mat_m[0] = [
+                            img[int((j*h+2)-(y-y//1+1)),int((i*h+2)-(x-x//1+1)),channel],  
+                            img[int((j*h+2)-(y-y//1)),int((i*h+2)-(x-x//1+1)),channel],
+                            img[int((j*h+2)+(y//1-y+1)),int((i*h+2)-(x-x//1+1)),channel], 
+                            img[int((j*h+2)+(y//1-y+2)),int((i*h+2)-(x-x//1+1)),channel]
+                            ]
+                mat_m[1] = [
+                        img[int((j*h+2)-(y-y//1+1)),int((i*h+2)-(x-x//1)),channel],
+                        img[int((j*h+2)-(y-y//1+1)),int((i*h+2)-(x-x//1)),channel],
+                        img[int((j*h+2)+(y//1-y+1)),int((i*h+2)-(x-x//1)),channel],
+                        img[int((j*h+2)+(y//1-y+2)),int((i*h+2)-(x-x//1)),channel]]
+                mat_m[2] = [
+                            img[int((j*h+2)-(y-y//1+1)),int((i*h+2)+(x//1-x+1)),channel],
+                            img[int((j*h+2)-(y-y//1+1)),int((i*h+2)+(x//1-x+1)),channel],
+                            img[int((j*h+2)+(y//1-y+1)),int((i*h+2)+(x//1-x+1)),channel],
+                            img[int((j*h+2)+(y//1-y+2)),int((i*h+2)+(x//1-x+1)),channel]]
+                mat_m[3] = [
+                            img[int((j*h+2)-(y-y//1+1)),int((i*h+2)+(x//1-x+2)),channel],
+                            img[int((j*h+2)-(y-y//1+1)),int((i*h+2)+(x//1-x+2)),channel],
+                            img[int((j*h+2)+(y//1-y+1)),int((i*h+2)+(x//1-x+2)),channel],
+                            img[int((j*h+2)+(y//1-y+2)),int((i*h+2)+(x//1-x+2)),channel]
+                            ]               
+                _s1 = np.dot(np.array([[convMask((x-x//1+1)),convMask((x-x//1)),convMask((x//1-x+1)),convMask((x//1-x+2))]]), mat_m)
+                _s2 = np.dot(_s1,np.array([[convMask((y-y//1+1))],[convMask((y-y//1))],[convMask((y//1-y+1))],[convMask((y//1-y+2))]]))
+                dst[j, i, channel] = _s2
     return dst
 
 if __name__ == '__main__':
